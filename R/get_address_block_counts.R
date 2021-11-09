3a3f955f775b6676821eb7a5a2eaa8ef110be5ad
Aaron Schroeder <dads2busy@gmail.com>
2021-11-09 12:59:27 -0500

    added example to function

--- R/get_address_block_counts.R
+++ R/get_address_block_counts.R
@@ -9,20 +9,24 @@
 #' @export
 #' @examples
 #' get_address_block_counts("AL")
-# # A tibble: 371,954 × 4
-# # geoid            year measure             value
-# # <chr>           <dbl> <chr>               <dbl>
-# # 1 010010201001000  2021 total_housing_units     8
-# # 2 010010201001001  2021 total_housing_units    19
-# # 3 010010201001002  2021 total_housing_units    17
-# # 4 010010201001003  2021 total_housing_units    11
-# # 5 010010201001004  2021 total_housing_units     0
-# # 6 010010201001005  2021 total_housing_units     0
-# # 7 010010201001006  2021 total_housing_units     1
-# # 8 010010201001007  2021 total_housing_units     0
-# # 9 010010201001008  2021 total_housing_units     0
-# # 10 010010201001009  2021 total_housing_units     0
-# # … with 371,944 more rows
+#' # $data
+#' # A tibble: 371,954 × 4
+#' # geoid            year measure             value
+#' # <chr>           <dbl> <chr>               <dbl>
+#' # 1 010010201001000  2021 total_housing_units     8
+#' # 2 010010201001001  2021 total_housing_units    19
+#' # 3 010010201001002  2021 total_housing_units    17
+#' # 4 010010201001003  2021 total_housing_units    11
+#' # 5 010010201001004  2021 total_housing_units     0
+#' # 6 010010201001005  2021 total_housing_units     0
+#' # 7 010010201001006  2021 total_housing_units     1
+#' # 8 010010201001007  2021 total_housing_units     0
+#' # 9 010010201001008  2021 total_housing_units     0
+#' #10 010010201001009  2021 total_housing_units     0
+#' # … with 371,944 more rows
+#' # 
+#' # $metadata
+#' # {"displayName":["Citation Metadata"],"fields":[{"typeName":"title","multiple":false,"typeClass":"primitive","value":["Social Data Commons - U.S. Census Block Address Counts"]},{"typeName":"subtitle","multiple":false,"typeClass":"primitive","value":["2021 Count of Housing Units and Group Quarters per Census Block in the U.S."]},{"typeName":"author","multiple":true,"typeClass":"compound","value":[{"authorName":{"typeName":"authorName","multiple":false,"typeClass":"primitive","value":"Schroeder, Aaron"},"authorIdentifierScheme":{"typeName":"authorIdentifierScheme","multiple":false,"typeClass":"controlledVocabulary","value":"ORCID"}}]},{"typeName":"datasetContact","multiple":true,"typeClass":"compound","value":[{"datasetContactName":{"typeName":"datasetContactName","multiple":false,"typeClass":"primitive","value":"Schroeder, Aaron"}}]},{"typeName":"dsDescription","multiple":true,"typeClass":"compound","value":[{"dsDescriptionValue":{"typeName":"dsDescriptionValue","multiple":false,"typeClass":"primitive","value":"2021 Count of Housing Units and Group Quarters per Census Block in the U.S."}}]},{"typeName":"subject","multiple":true,"typeClass":"controlledVocabulary","value":["Social Sciences (Ex: Education, Politics, Sociology, Economics, Psychology)"]},{"typeName":"productionDate","multiple":false,"typeClass":"primitive","value":["2021-11-04"]},{"typeName":"depositor","multiple":false,"typeClass":"primitive","value":["Schroeder, Aaron"]},{"typeName":"dateOfDeposit","multiple":false,"typeClass":"primitive","value":["2021-11-04"]}]} 
 get_address_block_counts <- function(state_abbrv = "AL", outdir = "") {
   srv <- "dataverse.lib.virginia.edu"
   doi <- "doi:10.18130/V3/NAZO4B"
@@ -57,6 +61,4 @@ get_address_block_counts <- function(state_abbrv = "AL", outdir = "") {
     jsonlite::write_json(d, paste0(outdir, "/", meta_file_name), pretty = TRUE)
   }
 }
-
-# pkg <- get_address_block_counts(outdir = "data-raw")
 
